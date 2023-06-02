a069d3baaa3ffe676ba59bd1f3864246c4f90fec
012kirby210 <012kirby210@gmail.com>
2023-06-02 18:14:35 +0200

    Avec la DB. [ pre opti ]

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Thing = require('./models/Things');
+
 
 const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/oc_men_back')
+    .then(()=>console.log('connection ok'))
+    .catch(()=>console.log('Failed to connect'));
 
 app.use(express.json());
 app.use( (req, res, next)=>{
@@ -13,29 +19,40 @@ app.use( (req, res, next)=>{
 });
 
 app.post('/api/stuff', (req, res, next) =>{
-  console.log(req.body);
-  res.status(201).json({message: 'objet crée'});
+  delete req.body._id;
+
+  const thing = new Thing({
+    ...req.body,
+  });
+  thing.save()
+      .then(()=>{
+        res.status(201).json({message: 'objet crée'});
+      })
+      .catch((error)=>res.status(400).json({error}));
+});
+
+app.put('/api/stuff/:id', (req, res, next)=>{
+  Thing.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+      .then( (thing)=>res.status(200).json(thing))
+      .catch( (error)=>res.status(400).json({error}));
+});
+
+app.delete('/api/stuff/:id', (req, res, next)=>{
+  Thing.deleteOne({_id: req.params.id})
+      .then( ()=>res.status(200).json({message: 'Objet supprimé'}))
+      .catch( (error)=>res.status(404).json({error}));
+});
+
+app.get('/api/stuff/:id', (req, res, next)=>{
+  Thing.findOne({_id: req.params.id}).
+      then((thing)=>res.status(200).json(thing))
+      .catch((error)=>res.status(404).json({error}));
 });
 app.get( '/api/stuff', (req, res, next) =>{
-  const stuff = [
-    {
-      _id: 'oeihfzeoi',
-      title: 'Mon premier objet',
-      description: 'Les infos de mon premier objet',
-      imageUrl: '',
-      price: 4900,
-      userId: 'gsomihvqios',
-    },
-    {
-      _id: 'oeihfzeomoihi',
-      title: 'Mon deuxième objet',
-      description: 'Les infos de mon deuxième objet',
-      imageUrl: '',
-      price: 2900,
-      userId: 'qsomihbqios',
-    },
-  ];
-  res.status(200).json(stuff);
+  Thing.find().then((things)=>{
+    res.status(200).json(things);
+  })
+      .catch((error)=>res.status(400).json({error}));
 });
 
 module.exports = app;
